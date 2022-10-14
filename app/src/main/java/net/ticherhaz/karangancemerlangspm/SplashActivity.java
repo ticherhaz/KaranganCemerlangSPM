1b44638955a272839884286f9293020199e966df
Ticherhaz <ticherhaz@gmail.com>
2022-10-14 16:43:52 +0800

    KCSPM 3.42

--- app/src/main/java/net/ticherhaz/karangancemerlangspm/SplashActivity.java
+++ app/src/main/java/net/ticherhaz/karangancemerlangspm/SplashActivity.java
@@ -183,8 +183,8 @@ public class SplashActivity extends AppCompatActivity {
                             toast.show();
                         }
                         //After that, we chat the value
-                        else if (system.getVersi() == 341) {
-                            //TODO: Version right now is 341. Please update when the new version is released. testing for beta forum (44)
+                        else if (system.getVersi() == 342) {
+                            //TODO: Version right now is 342. Please update when the new version is released. testing for beta forum (44)
                             //If all the condition above is met, it will NOT GOING THIS PART INSTEAD THEY WILL GO OUTSIDE FROM THE onDataChange
                             //It not met, then it will proceed here.
                             storeUserInfo(userUid);
